a37761624ea26cab5bdad6ef59d7e3377be3b6e9
hailay12michael <haile12black@gmail.com>
2023-12-04 23:03:23 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fastapi_weather-app
+ create a weather website or app that is both beautiful and functional
--- scrap.py
+++ scrap.py
@@ -1,4 +1,11 @@
 import requests
-
+def getTemp():
 r=requests.get("https://api.weather.gov/gridpoints/TOP/31,80/forecast").json()
-print(r)
+day_temp= r["properties"]["period"]{0}
+return {
+   "temperature":day_temp["temperature"]
+ "wind_speed":day_temp["windspeed"]
+ "short_forcast":day_temp["shortforcast"]
+ "icon":day_temp["icon"]
+}
+print(getTemp())
